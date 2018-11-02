b13c0933db38af6b6cb3f4e3b04105afdbc3c567
Juliocesarramirez <julio_marshal@hotmail.com>
2018-11-02 14:04:49 -0600

    feat(readme): added doncan profile, and method service

---
+++ src/app/method.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { url } from 'inspector';
+
+@Injectable()
+export class MethodService {
+
+  public api: string;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+  //this method will retunr the token
+  getAuthToken() {
+    return localStorage.getItem('token');
+  }
+
+  /**
+   * Perfors any type of http request
+   * @param urlServices
+   * @param query
+   * @return {Response}
+   */
+
+  public get(urlServices: string, query: any) {
+
+    let getUrl;
+
+    if (query) {
+      getUrl = `${urlServices}?filter=${query}`;
+    } else {
+      getUrl = urlServices;
+    }
+
+    return this.http.get(this.api + getUrl);
+
+  }
+
+  /**
+  * Perfors any type of http request
+  * @param urlServices
+  * @param query
+  * @return {Response}
+  */
+
+  getOne(urlServices: string, id: string, query: any) {
+
+    let getUrl;
+
+    if (query) {
+      getUrl = `${urlServices}/${urlServices}/?filter=${query}`;
+    } else {
+      getUrl = `${urlServices}/${id}`;
+    }
+
+    return this.http.get(this.api + getUrl);
+
+  }
+
+  /**
+  * Perfors any type of http request
+  * @param urlServices
+  * @param data
+  * @return {Response}
+  */
+
+  post(urlServices: string, id:string, data:any){
+    return this.http.put(this.api + `${urlServices}/${id}`, data)
+  }
+
+  /**
+  * Perfors any type of http request
+  * @param urlServices
+  * @param id
+  * @return {Response}
+  */
+
+  delete(urlServices: string, id: string){
+    return this.http.delete(this.api + `${urlServices}/${id}`)
+  }
+
+}
