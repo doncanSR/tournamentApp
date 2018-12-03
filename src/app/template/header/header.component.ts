7383989907bba5778b08d805ef839661bd16b8ed
Juliocesarramirez <julio_marshal@hotmail.com>
2018-12-03 06:30:41 -0600

    feat(header): changed header componet

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +8,23 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
-
+  logged: boolean = false;
   @Input() title : string;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.isLogged();
+  }
+  doLogin(){
+    this.router.navigateByUrl('/login');
   }
 
+  isLogged(){
+    if (localStorage.TOKEN) {
+      this.logged = true;
+    }
+  }
 }
