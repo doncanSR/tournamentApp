af3d89322815bef7b74aa1b96c476504f055216b
Juliocesarramirez <julio_marshal@hotmail.com>
2018-12-26 21:21:31 -0600

    feat(tabs): added table to show results

--- src/app/tabs/results/result.page.ts
+++ src/app/tabs/results/result.page.ts
@@ -5,4 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: 'result.page.html',
   styleUrls: ['result.page.scss']
 })
-export class ResultPage {}
+export class ResultPage {
+  headerColumns: any = ['Equipo', 'Puntos', 'Lugar']
+  table: any =
+    [
+      {
+        name: 'Equipo 1',
+        points: 76,
+        position: 1
+      },
+      {
+        name: 'Equipo 2',
+        points: 66,
+        position: 2
+      },
+      {
+        name: 'Equipo 3',
+        points: 46,
+        position: 3
+      },
+      {
+        name: 'Equipo 4',
+        points: 36,
+        position: 4
+      }
+    ]
+}
