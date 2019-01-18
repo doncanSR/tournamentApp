71eb31afd6ceb8f0a6910f90b01a1c9aa0cb272e
Juliocesarramirez <julio_marshal@hotmail.com>
2019-01-17 20:45:47 -0600

    feat(game, team): Added layout and form

--- src/app/team/registry-team/registry-team.page.ts
+++ src/app/team/registry-team/registry-team.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MethodService } from '../../utils/http/method.service';
 
 @Component({
   selector: 'app-registry-team',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registry-team.page.scss'],
 })
 export class RegistryTeamPage implements OnInit {
+  newTeam: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private methodService: MethodService
+  ) { }
 
   ngOnInit() {
+    this.newTeam = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      ponints: ['', [Validators.required]],
+      playersNo: ['', [Validators.required]],
+      from: ['', [Validators.required]],
+      coachId: ['', [Validators.required]]
+    });
+  }
+
+  get f() { return this.newTeam.controls; } 
+  createTeam(): void{
+    this.submitted = true;
+
+    if (this.newTeam.invalid) {
+      return;
+    } else {
+      console.log('Data ----> ', this.newTeam.value);
+      
+    }
   }
 
 }
