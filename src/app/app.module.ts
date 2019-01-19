0c627ef5c00a8972ecf5cc4a320e859d8da9a506
Juliocesarramirez <julio_marshal@hotmail.com>
2019-01-19 13:16:39 -0600

    feat(app): added new services for each module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,21 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+//Services
 import { InterceptService} from './utils/intercept.service';
-
+import { AuthService } from "./auth/service/auth.service";
+import { MethodService } from './utils/http/method.service';
+import { GameService } from "./game/service/game.service";
+import { ManagerService } from "./managers/service/manager.service";
+import { PlayerService } from "./player/service/player.service";
+import { RefereeService } from "./referee/service/referee.service";
+import { TeamService } from "./team/service/team.service";
+import { TournamentService } from "./tournament/service/tournament.service";
 //Module core
 import { AuthModule } from "./auth/auth.module";
 import { AppRoutingModule } from './app-routing.module';
 import { TabsPageModule } from "./tabs/tabs.module";
 import { AppComponent } from './app.component';
-import { MethodService } from './utils/http/method.service';
 import { TemplateModule } from './template/template.module';
 import { GameModule } from "./game/game.module";
 import { ManagersModule } from "./managers/managers.module";
@@ -69,6 +76,13 @@ export function token(){
     },
     SplashScreen,
     MethodService,
+    GameService,
+    PlayerService,
+    ManagerService,
+    TeamService,
+    TournamentService,
+    RefereeService,
+    AuthService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
