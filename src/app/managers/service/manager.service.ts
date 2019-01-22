5b6fc286d58fc3b63136f5cc16fc80592c47473c
Juliocesarramirez <julio_marshal@hotmail.com>
2019-01-21 21:11:23 -0600

    feat(manager): added manager and capturist service

--- src/app/managers/service/manager.service.ts
+++ src/app/managers/service/manager.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { MethodService } from '../../utils/http/method.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManagerService {
 
-  constructor() { }
+  constructor(
+    private methodService: MethodService
+  ) { }
+
+  addManager(manager: any) {
+    return this.methodService.post('role', manager)
+  }
+
+  addCapturist(capturist: any) {
+    return this.methodService.post('capturist', capturist)
+  }
+
 }
